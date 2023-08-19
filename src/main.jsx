7d6f1a7d0da6722f97e12a89e0565718e65cd1e3
Alolonso <joseloso1181mana04@gmail.com>
2023-08-19 04:56:01 -0600

    Changing BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import { CvProvider } from './components/CvContext'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CvProvider>
         <App/>
       </CvProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
